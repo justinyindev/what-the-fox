4c36e36c85831c1a8b43f92d5ac1882cce0f3779
Justin Yin <zzyin@uwaterloo.ca>
2023-03-02 22:07:02 -0500

    added server cache

--- server/utils/getArticleContent.js
+++ server/utils/getArticleContent.js
@@ -1,35 +1,37 @@
 const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
 
 const getArticleContent = async (url) => {
+  const cacheRes = cache.get(url);
+  if (cacheRes) {
+    return cacheRes;
+  }
+
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
   );
-  
+
   await page.goto(url);
 
-  // Wait for the <div> element with class c-entry-content to be loaded
-  await page.waitForSelector(".c-entry-content");
+  const htmlContent = await page.content();
+  const $ = cheerio.load(htmlContent);
 
   // Get the content of the <div> element with class c-entry-content
-  const articleContent = await page.evaluate(() => {
-    // Get all of the <p> and <h3> tags under the <div> element with class c-entry-content
-    const paragraphs = [
-      ...document.querySelectorAll(".c-entry-content p"),
-    ];
-
-    const filteredParagraphs = paragraphs.filter(
-      (p) => !p.closest(".c-article-footer")
-    );
-
-    // Join the text content of the paragraphs with a newline character
-    const content = filteredParagraphs.map((p) => p.textContent).join("\n");
-
-    return content;
-  });
+  const articleContent = $(".c-entry-content")
+    .find("p,h3")
+    .filter((i, el) => !$(el).closest(".c-article-footer").length)
+    .map((i, el) => $(el).text())
+    .get()
+    .join("\n");
 
   await page.close();
+  await browser.close();
+
+  cache.set(url, articleContent);
 
   return articleContent;
 };

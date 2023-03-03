2ce2c0a145b0e61006fd53ede08f692fe8d4a570
Justin Yin <zzyin@uwaterloo.ca>
2023-03-02 21:25:02 -0500

    UI update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import "./static/css/App.css";
 import NewsCard from "./components/newsCard";
 import Calendar from "./components/calendar";
+import logo from "./static/images/wtv2.png";
 
 function App() {
   return (
     <div className="homepage">
+      <img className="homepage-banner-logo" src={logo} alt="logo" />
+      <div className="homepage-banner"></div>
       <NewsCard />
       <Calendar />
     </div>

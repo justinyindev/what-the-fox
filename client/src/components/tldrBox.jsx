3c31fd6d271f86779e2d7d4332434a35597587a6
Justin Yin <zzyin@uwaterloo.ca>
2023-03-04 16:09:18 -0500

    ui template tldr

--- client/src/components/tldrBox.jsx
+++ client/src/components/tldrBox.jsx
@@ -5,6 +5,7 @@ import "./../static/css/tldrBox.css";
 
 const TldrBox = ({ item }) => {
   const [summary, setSummary] = useState("");
+  const [showTldr, setShowTldr] = useState(false);
   const [summaryLoading, setLoadingSummary] = useState(false);
 
   const fetchSummary = async (item) => {
@@ -35,21 +36,44 @@ const TldrBox = ({ item }) => {
         className="tldr-button"
         onClick={() => {
           handleSummarize(item);
+          setShowTldr(true);
         }}
         disabled={summaryLoading}
       >
         TL;DR
       </button>
-      {summaryLoading ? (
-        <Typewriter
-          options={{
-            strings: ["Scraping article", "summarizing..."],
-            autoStart: true,
-            loop: true,
-          }}
-        />
+      {!showTldr ? (
+        <div className="tldr-text-container">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(2500)
+                .typeString("A simple yet powerful native javascript")
+                .pauseFor(300)
+                .deleteChars(10)
+                .typeString(
+                  "<strong>JS</strong> plugin for a cool typewriter effect and "
+                )
+                .typeString(
+                  '<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>'
+                )
+                .pauseFor(1000)
+                .start();
+            }}
+          />
+        </div>
+      ) : summaryLoading ? (
+        <div className="tldr-text-container">
+          <Typewriter
+            options={{
+              strings: ["Scraping article", "summarizing..."],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
       ) : (
-        summary && <p className="tldr-text fade-in">{summary}</p>
+        summary && <p className="tldr-text">{summary}</p>
       )}
     </div>
   );

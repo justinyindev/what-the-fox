82ab17eed485ceca385680519a6d81bf498fb456
Justin Yin <zzyin@uwaterloo.ca>
2023-03-04 20:10:12 -0500

    marquee

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./static/css/App.css";
 import HomePage from "./components/homePage";
 import Calendar from "./components/calendar";
 import logo from "./static/images/wtv2.png";
+import Marquee from "react-fast-marquee";
+import { getHeadlines } from "./utils/apiService";
 
 function App() {
+  const [currentHeadline, setCurrentHeadline] = useState("");
+
+  useEffect(() => {
+    const fetchHeadlines = async () => {
+      try {
+        const response = await getHeadlines();
+        console.log(response[0].title);
+        const headline = response[0].title;
+        setCurrentHeadline(headline);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchHeadlines();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <img className="banner-logo" src={logo} alt="logo" />
-      <div className="navbar"></div>
+      <div className="banner">
+        <Marquee
+          className="banner-marquee"
+          speed={75}
+          gradient={false}
+          pauseOnHover={true}
+        >
+          <h1 className="banner-marquee-headline">{currentHeadline}</h1>
+        </Marquee>
+      </div>
       <div className="calendar">
         <Calendar />
       </div>
